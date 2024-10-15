328a6c39c77b12f840e102c75ed340d96e6c4f5e
Vetri Selvam <vetriwork1@gmail.com>
2024-10-15 15:23:19 +0530

    Add user table with CRUD actions using BlueprintJS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
+import { Button } from "@blueprintjs/core";
+import "./App.css";
 import './App.css';
 
 function App() {
+    const [users, setUsers] = useState([]);
+    useEffect(
+        ()=>{
+            fetch('https://jsonplaceholder.typicode.com/users')
+            .then( (response) => response.json() )
+            .then( (json) => setUsers(json))
+            //https://jsonplaceholder.typicode.com/todos/users
+        }
+        ,[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className="bpr-html-table modifier">
+        <thead>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Website</th>
+            <th>Action</th>
+        </thead>
+        <tbody>
+            {users.map(user =>
+                <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.website}</td>
+                    <td>
+                        <Button intent="primary">Update</Button>
+                        <Button intent="danger">Delete</Button>
+                    </td>
+                </tr>
+            )}
+        </tbody>
+      </table>
     </div>
   );
 }
